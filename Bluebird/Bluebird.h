5499d25672dfb55a3fe32ef4f13e822005bedd2c
Alejandro Barros Cuetos <jandrob1978@gmail.com>
2015-02-03 20:04:26 +0000

    Initial Commit

---
+++ Bluebird/Bluebird.h
@@ -0,0 +1,19 @@
+//
+//  Bluebird.h
+//  Bluebird
+//
+//  Created by Alejandro Barros on 03/02/15.
+//  Copyright (c) 2015 Alejandro Barros Cuetos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bluebird.
+FOUNDATION_EXPORT double BluebirdVersionNumber;
+
+//! Project version string for Bluebird.
+FOUNDATION_EXPORT const unsigned char BluebirdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bluebird/PublicHeader.h>
+
+
